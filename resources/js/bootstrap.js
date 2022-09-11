05911441c8e30669ce2f82d487c9a2c4c343ad64
Karan Sanjeev <yo@alphaman.me>
2022-09-11 19:38:28 +0530

    Add fontawesome

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,6 +12,9 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+import '@fortawesome/fontawesome-free/js/solid.min'
+import '@fortawesome/fontawesome-free/js/fontawesome.min'
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
