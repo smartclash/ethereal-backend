66c17eac8d46ea8566248018047d32e95455935f
Karan Sanjeev <yo@alphaman.me>
2022-09-12 15:15:05 +0530

    Update image and add resume

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,9 @@
 <footer class="footer has-background-warning-light">
     <div class="content has-text-centered mb-5">
+        <p>If you need any assistance, contact <a href="mailto:hey@kcgethereal.com">hey@kcgethereal.com</a></p>
         <p>
             Website developed by <a href="https://alphaman.me/" target="_blank">Karan Sanjeev Nair</a>.
+            View my resume by <a href="{{ asset('files/karan-resume.pdf') }}" target="_blank">clicking here</a>.
         </p>
     </div>
 </footer>
